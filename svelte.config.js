4c782585d2400ce890317e8a89e700884db80c97
Kaiting Chen <ktchen14@gmail.com>
2025-07-21 18:27:58 -0400

    Configure appDir

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter(),
+    appDir: 'svelte',
     files: {
       appTemplate: 'template.html',
       lib: 'common',
