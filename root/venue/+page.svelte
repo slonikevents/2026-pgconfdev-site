83a78182ff578928d201776d8961d7559aa69737
Steve Singer <steve@ssinger.info>
2025-08-02 14:18:26 -0400

    Add venue information

--- root/venue/+page.svelte
+++ root/venue/+page.svelte
@@ -1,3 +1,119 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
 <h1>Venue</h1>
 
-<p>Simon Fraser University—Vancouver Campus in Vancouver, BC, Canada</p>
+<p>
+  PostgreSQL Development Conference 2026 will be held at the Vancouver Campus of
+  Simon Fraser University in Vancouver, Canada.
+</p>
+
+<p>
+  Address: Harbour Centre, 515 West Hastings Street Vancouver, B.C. Canada V6B
+  5K3
+</p>
+<p>
+  The Vancouver Campus of Simon Fraser University, is located in the heart of
+  downtown Vancouver near Waterfront Station and Gastown.
+</p>
+<iframe
+  title="SFU Downtown"
+  width="425"
+  height="350"
+  src="https://www.openstreetmap.org/export/embed.html?bbox=-123.15562248229982%2C49.26281971048906%2C-123.06850433349611%2C49.30576242115396&amp;layer=mapnik&amp;marker=49.284295740572944%2C-123.11206340789795"
+  style="border: 1px solid black"
+></iframe><br /><small
+  ><a
+    href="https://www.openstreetmap.org/?mlat=49.2843&amp;mlon=-123.1121#map=14/49.2843/-123.1121"
+    >View Larger Map</a
+  ></small
+>
+<p>
+  Simon Fraser University has multiple campuses in the Vancouver area. The
+  conference is being held at the <b>Vancouver</b> campus which is smaller
+  downtown near Waterfront station. The conference is <b>not</b> at the main Burnbay
+  campus
+</p>
+
+<h2>Floor plans</h2>
+
+<p>
+  The conference is being held in the <b>Harbour Center</b> in the following rooms(among
+  others)
+</p>
+
+<h3 id="fletcher">Fletcher Challenge Canada Theatre</h3>
+<p>
+  The Fletcher Challenge Canada Theatre is room 1900 is located on the main
+  floor.
+</p>
+
+<h3 id="segal">Segal Centre</h3>
+<p>The Segal Centre is rooms 1400-1410 is located on the main floor</p>
+
+<h3 id="cominco">Labatt Hallt</h3>
+<p>The Labatt Hall 1700 is located on the main floor</p>
+
+<h2>Travel</h2>
+<p>
+  Vancouver International Airport (YVR) is the closest airport and has flights
+  to many international destinations.
+</p>
+
+<h4>Canada Line from YVR to the Conference</h4>
+<p>
+  The best way to get from Vancouver International Airport to the Conference is
+  to take the <a href="http://thecanadaline.com">Canada Line(Skytrain)</a> from the
+  airport to Waterfront station. The Harbour Centre is directly across the sreet
+  from the Waterfront train station
+</p>
+
+<h2>Visa Information</h2>
+<p>
+  Travellers from many countries need either a vistor visa or an Electronic
+  Travel Authorization (eTA). You can use
+  <a href="https://ircc.canada.ca/english/visit/visas.asp">this tool</a> to determine
+  which applies to you.
+</p>
+
+<h3>U.S. Citizens or permanent residents</h3>
+<p>
+  U.S. Citizen must have a valid passport to enter Canada. U.S. lawful permanent
+  residents must have the documents described
+  <a
+    href="https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/entry-requirements-country.html#lawful-pr-us"
+    >here</a
+  >
+</p>
+
+<h3>Electronic Travel Authorization(eTA)</h3>
+<p>
+  Visitors from <a href="https://ircc.canada.ca/english/visit/visas.asp"
+    >visa-exempt countries</a
+  >
+  need to apply for an eTa. The
+  <a
+    href="https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/eta/apply.html"
+  >
+    eTa site</a
+  > contains instructions for applying for an eTa. There is a fee for this but most
+  applications are approved quickly.
+</p>
+
+<h3>Visitor visa</h3>
+<p>
+  Procedures for obtaining a visitor visa are available from
+  <a
+    href="https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/about-visitor-visa.html"
+  >
+    What is a visitor visa</a
+  >. This process can be lengthy and we advise that you start it as soon as you
+  have decided that you will be attending the conference.
+</p>
+
+<h2>Questions</h2>
+<p>
+  If you have any questions regarding your travel arrangements feel free to
+  <a href="{base}/contact">contact us</a>, we are more than happy to assist you.
+</p>
