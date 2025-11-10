1854fd618319c4d27b29d495d235532ed26f29cb
Kaiting Chen <ktchen14@gmail.com>
2025-11-10 11:35:01 -0500

    Add EDB to new sponsors page

--- root/sponsor/+page.server.js
+++
@@ -1,6 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import { resolve } from '$app/paths';
-
-export function load() {
-  redirect(301, resolve('/sponsor/levels'));
-}
---
+++ root/sponsor/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import EDB from '../logo/EDB.svg';
+</script>
+
+<svelte:head>
+  <title>Sponsors of PGConf.dev 2026</title>
+  <meta
+    name="description"
+    content="PGConf.dev is an annual event where users, developers, and community organizers come together to focus on PostgreSQL development and community growth. This event is a strategic gathering of the PostgreSQL community that takes place before development begins."
+  />
+</svelte:head>
+
+<h1 style="color: #b78800;">Gold Sponsors</h1>
+
+<h2><img src={EDB} alt="EDB" /></h2>
+<p>
+  As the leading contributor to PostgreSQL, EDB is committed to building the
+  next generation of Postgres for operators, developers, data engineers, and AI
+  builders. EDB provides a modern Postgres data platform for transactional,
+  analytical, and new AI workloads powered by an enhanced Postgres engine.
+</p>
