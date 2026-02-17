b28b89df5d00950eea7d5a95c9f6b90a09cd5084
Melanie Plageman <melanieplageman@gmail.com>
2026-02-17 13:08:39 -0500

    Schedule Updates

--- root/schedule/thursday/+page.server.js
+++ root/schedule/thursday/+page.server.js
@@ -4,14 +4,71 @@ export async function load({ parent }) {
   const data = await parent();
   const schedule = data.schedule.find(({ date }) => date.dayOfWeek == 4);
 
-  let roster = schedule.roster.concat({
-    slot: new Slot(
-      schedule.date.toPlainDateTime('12:30'),
-      schedule.date.toPlainDateTime('14:00'),
-    ),
-    name: 'Lunch',
-    span: [0, 2],
-  });
+  let roster = schedule.roster.concat(
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('08:30'),
+        schedule.date.toPlainDateTime('09:30'),
+      ),
+      name: 'Breakfast',
+      room: 'Concourse',
+      span: [0, 3],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('10:30'),
+        schedule.date.toPlainDateTime('11:00'),
+      ),
+      name: 'Coffee',
+      room: 'Concourse',
+      span: [0, 3],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('13:45'),
+        schedule.date.toPlainDateTime('14:00'),
+      ),
+      name: "Major Contributors' Group Photo",
+      room: 'Fletcher',
+      span: [0, 0],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('12:30'),
+        schedule.date.toPlainDateTime('14:00'),
+      ),
+      name: 'Lunch',
+      room: 'Concourse',
+      span: [1, 1],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('12:30'),
+        schedule.date.toPlainDateTime('13:45'),
+      ),
+      name: 'Poster Session Showcase',
+      room: 'Segal',
+      span: [2, 2],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('15:30'),
+        schedule.date.toPlainDateTime('16:15'),
+      ),
+      name: 'Coffee + Cake Cutting',
+      room: 'Concourse',
+      span: [0, 3],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('18:15'),
+        schedule.date.toPlainDateTime('21:15'),
+      ),
+      name: 'Meet + Eat',
+      room: 'Concourse',
+      span: [0, 3],
+    },
+  );
 
   roster.find(({ name }) => name == 'Lightning Talks').span = [0, 3];
 
--- root/schedule/wednesday/+page.server.js
+++ root/schedule/wednesday/+page.server.js
@@ -4,17 +4,57 @@ export async function load({ parent }) {
   const data = await parent();
   const schedule = data.schedule.find(({ date }) => date.dayOfWeek == 3);
 
-  let roster = schedule.roster.concat({
-    slot: new Slot(
-      schedule.date.toPlainDateTime('12:00'),
-      schedule.date.toPlainDateTime('13:00'),
-    ),
-    name: 'Lunch',
-    span: [0, 3],
-  });
+  let roster = schedule.roster.concat(
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('07:30'),
+        schedule.date.toPlainDateTime('08:30'),
+      ),
+      name: 'Breakfast',
+      room: 'Concourse',
+      span: [0, 1],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('10:00'),
+        schedule.date.toPlainDateTime('10:30'),
+      ),
+      name: 'Coffee',
+      room: 'Concourse',
+      span: [0, 2],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('12:00'),
+        schedule.date.toPlainDateTime('13:00'),
+      ),
+      name: 'Lunch',
+      room: 'Concourse',
+      span: [0, 2],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('15:00'),
+        schedule.date.toPlainDateTime('15:30'),
+      ),
+      name: 'Coffee',
+      room: 'Concourse',
+      span: [0, 2],
+    },
+    {
+      slot: new Slot(
+        schedule.date.toPlainDateTime('18:00'),
+        schedule.date.toPlainDateTime('21:00'),
+      ),
+      name: 'Social',
+      room: 'Rogue',
+      span: [0, 2],
+    },
+  );
 
-  roster.find(({ name }) => name == 'Wednesday Opening').span = [0, 3];
-  roster.find(({ name }) => name.startsWith('Group Photo')).span = [0, 3];
+  roster.find(({ name }) => name == 'Opening Remarks').span = [0, 2];
+  roster.find(({ name }) => name.startsWith('Group Photo')).span = [0, 2];
+  roster.find(({ name }) => name.startsWith('30 Years')).span = [0, 2];
 
   return { ...schedule, roster };
 }
