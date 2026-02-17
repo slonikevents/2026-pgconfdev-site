b2061f73422dec280f64c765e7db103b6ba10a48
Melanie Plageman <melanieplageman@gmail.com>
2026-02-17 14:26:21 -0500

    fixup 2

--- root/schedule/tuesday/+page.svelte
+++ root/schedule/tuesday/+page.svelte
@@ -6,5 +6,13 @@
 </script>
 
 <DateHead date={data.date} />
+<p>
+  The schedule below shows only a subset of what will be happening on Tuesday!
+</p>
+<p>
+  Tuesday will feature a large number of community discussion sessions, many of
+  which will be open to all conference attendees. These will gradually be added
+  to this schedule as we get closer to the event.
+</p>
 
 <Schedule layout={data.layout} roster={data.roster} />
