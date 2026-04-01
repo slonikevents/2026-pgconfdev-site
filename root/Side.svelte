c665eb00b7c1f953cd52313ab0c8368b89663f67
Kaiting Chen <ktchen14@gmail.com>
2026-04-01 09:19:39 -0400

    Refactor sponsor sidebar

--- root/Side.svelte
+++ root/Side.svelte
@@ -14,60 +14,69 @@
     }
   }
 
-  @media (width < 768px) {
-    .gold {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 1rem;
-      justify-content: space-around;
-    }
-
-    .gold > * {
-      flex: 0 0 calc(50% - 0.5rem);
-      margin: 0;
-    }
+  section:where(:not(:last-child)) {
+    margin-block-end: 2rem;
   }
 
-  @media (width < 480px) {
-    .gold > * {
-      flex: 0 1 100%;
+  a {
+    border-radius: var(--border-radius);
+    display: block;
+    padding: 1rem;
+  }
+
+  .gold {
+    h4 {
+      color: var(--gold-color);
+    }
+
+    img {
+      max-height: 8rem;
     }
   }
 
   .silver {
-    margin: 10px 0;
+    h4 {
+      color: var(--silver-color);
+    }
+
+    img {
+      max-height: 4rem;
+    }
+
+    ul {
+      align-items: center;
+      display: grid;
+      gap: 1rem;
+      list-style-type: none;
+      padding-inline-start: 0;
+
+      @media (width < 768px) {
+        grid-template-columns: repeat(2, 1fr);
+      }
+    }
   }
 </style>
 
 <div id="side">
-  <h4 style="color: #b78800;">Gold Sponsors</h4>
+  <section class="gold">
+    <h4>Gold Sponsors</h4>
 
-  {#each SPONSOR_LIST.GOLD as sponsor (sponsor.NAME)}
-    <div class="gold">
+    {#each SPONSOR_LIST.GOLD as {NAME, LOGO, URL} (NAME)}
       <p>
-        <a
-          href={sponsor.URL}
-          target="_blank"
-          style="text-decoration: none; border: none; outline: none;"
-        >
-          <img src={sponsor.LOGO} alt={sponsor.NAME} />
-        </a>
+        <a href={URL} target="_blank"><img src={LOGO} alt={NAME} /></a>
       </p>
-    </div>
-  {/each}
-  <h4 style="color: #707070;">Silver Sponsors</h4>
+    {/each}
+  </section>
 
-  {#each SPONSOR_LIST.SILVER as sponsor (sponsor.NAME)}
-    <div class="silver">
-      <p>
-        <a
-          href={sponsor.URL}
-          target="_blank"
-          style="text-decoration: none; border: none; outline: none;"
-        >
-          <img src={sponsor.LOGO} alt={sponsor.NAME} />
-        </a>
-      </p>
-    </div>
-  {/each}
+  <section class="silver">
+    <h4>Silver Sponsors</h4>
+
+    <ul>
+      {#each SPONSOR_LIST.SILVER as {NAME, LOGO, URL} (NAME)}
+        <li>
+          <a href={URL} target="_blank"><img src={LOGO} alt={NAME} /></a>
+        </li>
+      {/each}
+    </ul>
+  </section>
 </div>
