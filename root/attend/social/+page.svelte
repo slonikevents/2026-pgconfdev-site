5034b1670295ce2175d9e7535678a8cbbfbf8ecf
Paul Ramsey <pramsey@cleverelephant.ca>
2026-04-29 14:26:50 -0700

    Add a Newcomer breakfast entry

--- root/attend/social/+page.svelte
+++ root/attend/social/+page.svelte
@@ -23,6 +23,13 @@
   <a href={resolve('/about/contact-us')}>contact us</a>.
 </p>
 
+<a name="breakfast" title="Breakfast"></a>
+<h2>Newcomer Welcome Breakfast (Tuesday)</h2>
+
+<p>
+  Is this your first in-person PostgreSQL event? Are you a new member of the community? Then <a href="/session/712">this breakfast</a> is for you! Meet other newcomers along with experienced members of the community who will welcome and orient you. <b>Tuesday, 7:30</b> in the Xerox Room (1500), <b>no signup required</b>.
+</p>
+
 <a name="meet" title="MeatEat"></a>
 <h2>Meet &amp Eat (Tuesday &amp; Thursday)</h2>
 
