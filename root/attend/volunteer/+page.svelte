0b2592c4cb4ccb7e42ce0ddc55dd78c163235a3e
Robert Haas <rhaas@postgresql.org>
2026-04-30 11:39:08 -0400

    volunteer directions

--- root/attend/volunteer/+page.svelte
+++ root/attend/volunteer/+page.svelte
@@ -14,7 +14,200 @@
   help with organizing work in advance of the conference either for this year or
   future years. If interested, please contact us at
   <a href="mailto:contact@pgconf.dev">contact@pgconf.dev</a> and let us know how you
-  would like to help.
+  would like to help. Directions for volunteers can be found below.
+</p>
+
+<h2>Registration Desk</h2>
+
+<p>
+  Please arrive a few minutes before the start of your shift, so that the
+  previous volunteers can leave on time, and so that you can get oriented before
+  you begin your shift. The main duties of registration desk volunteers are to
+  check-in attendees, distribute T-shirts, distribute sponsor-provided items,
+  answer questions (such as where the restrooms are, or where a particular room
+  is located), and to put attendees in touch with the organization team when
+  needed.
+</p>
+
+<p>
+  We will be using the conference system both to check in attendees and to track
+  T-shirt distribution. You should be able to access this system through your
+  <a href="https://www.pgevents.ca/events/pgconfdev2026/register"
+    >conference registration page</a
+  >. Before arriving in Vancouver, make sure you see this link. If you do not,
+  notify <a href="mailto:contact@pgconf.dev">contact@pgconf.dev</a>
+  or speak with a member of the organization team in person.
+</p>
+
+<p>
+  Once you have access, read through that page. If you will be using the Android
+  App for checkin processing and T-shirt distribution, install it on your phone.
+  Otherwise, decide whether you will be using the "Open By Link" method or the
+  "Native Scanning on Device" method and make sure you can access whichever one
+  you choose. To check in an attendee, you can either look them up by name or
+  you can scan their ticket, which they can access through their conference
+  registration desk. To mark an attendee as having received a T-shirt, you must
+  scan their badge, not their ticket.
+</p>
+
+<p>
+  We expect most attendees to check in on Tuesday or Wednesday morning.
+  Therefore, to ease the flow of traffic, we will not hand out T-shirts on those
+  day before 10 AM. Encourage people to return at a later time to pick up their
+  T-shirt. Attendees should be given the T-shirt size listed in their
+  registration. If it does not fit, it can be exchanged for a different size
+  beginning at 3 PM on Wednesday until supplies are exhausted. Make sure to scan
+  each attendee's badge when they pick up their T-shirt so that we know who has
+  received one.
+</p>
+
+<p>
+  Attendees should be encouraged to take the sponsor-provided items. We do not
+  keep track of which sponsor-provided items are taken by which attendees.
+</p>
+
+<p>
+  If an attendee has a question you can't answer or needs to speak to a member
+  of the organization team for any other reason, you can locate someone in
+  person or ping the <code>#onsite-volunteering</code> in the
+  <a href="https://discord.gg/GEfxA2ahQG">conference Discord</a>. If you can't
+  see this channel, join the server first, and then send your Discord username
+  to <a href="mailto:contact@pgconf.dev">contact@pgconf.dev</a>.
+</p>
+
+<h2>Room Hosts</h2>
+
+<p>
+  Please arrive at the room at the beginning of your shift, which should
+  correspond to 10 minutes before the first scheduled talk in that room.
+</p>
+
+<p>
+  Find and introduce yourself to the other room host for the scheduled slot.
+</p>
+
+<p>
+  Find and introduce yourself to the AV technician in the room. They should be
+  located near the back of the room and should be easily identifiable.
+</p>
+
+<p>
+  There are two separate jobs to do. Decide with the other room host which
+  person will handle which job.
+</p>
+
+<h3>Job A</h3>
+
+<p>Take the time cards, which should be located near the A/V technician.</p>
+
+<p>
+  Introduce yourself to the upcoming speaker. Help them get set up with the A/V
+  equipment and anything else they need.
+</p>
+
+<p>
+  Ask the speaker how they want to be introduced, and whether they want to allow
+  questions during the talk or only at the end.
+</p>
+
+<p>
+  Let them know that you will hold up cards to indicate how much time they have
+  left.
+</p>
+
+<p>
+  Indicate to them where you’ll be sitting. Put your time cards on that seat to
+  reserve it.
+</p>
+
+<p>
+  Remind the speaker to ask the audience to fill out feedback for this and all
+  other talks they attend. Feedback links are on the schedule page.
+</p>
+
+<p>
+  Introduce the speaker on time so that the session starts on schedule. If the
+  speaker doesn’t want to answer questions during the talk, inform the audience
+  to hold their questions until the end of the talk.
+</p>
+
+<p>
+  Don’t allow the audience to interrupt or heckle the speaker. If necessary,
+  remind them of the Code of Conduct.
+</p>
+
+<p>
+  Keep an eye on the clock and hold up the time cards at the appropriate times.
+</p>
+
+<p>
+  If there is time for questions and the speaker wants to answer questions,
+  bring the microphone to people so that they can be heard while asking
+  questions.
+</p>
+
+<p>
+  When time is up, if the audience is still asking questions, politely inform
+  them that the room must be cleared.
+</p>
+
+<p>
+  Remind the speaker to pick up their speaker gift at the registration desk.
+</p>
+
+<p>
+  Don’t forget to leave the time cards near the A/V technician before you leave
+  the room!
+</p>
+
+<h3>Job B</h3>
+
+<p>
+  Remain near the door to the room as people are entering, and make sure that it
+  remains open. If anyone needs help finding a seat, help direct them.
+</p>
+
+<p>
+  If a talk is popular, politely ask the audience to move toward the center of
+  the room. Look for open seats and ask people who’ve already sat down to shift
+  over. You may need to repeat this process as people enter the room.
+</p>
+
+<p>
+  Once the talk starts, quietly point late attendees toward open seats. If there
+  aren’t any open seats, inform them that they will have to stand in the back of
+  the room. If the speaker doesn’t want to answer the questions during the talk,
+  quietly inform late attendees that they should hold their questions until the
+  end of the talk.
+</p>
+
+<p>Once it looks like no one else will be joining the talk, close the door.</p>
+
+<p>
+  When time is up, open the door. If necessary, i.e. if people are talking
+  instead of exiting the room, politely inform the audience to quickly exit the
+  room in preparation for the next talk.
+</p>
+
+<h2>Photography</h2>
+
+<p>
+  Volunteers are encouraged to take photos during the event and to share them
+  via the conference's social media accounts. It is very important not to take
+  photos including anyone who is wearing a red lanyard. A red lanyard indicates
+  that this person has not consented to be photographed at this event.
+</p>
+
+<p>
+  To post photos, go to your
+  <a href="https://www.pgevents.ca/events/pgconfdev2026/register"
+    >conference registration page</a
+  >. For historical reasons, it is called "Twitter Posting". We no longer use
+  Twitter. You can use the web application found there to submit photos, and
+  they will be sent out via social media once approved by a member of the
+  organization team. If you do not see the "Twitter Posting" option on your
+  registration page, speak to a member of the organization team or email
+  <a href="mailto:contact@pgconf.dev">contact@pgconf.dev</a> for access.
 </p>
 
 <h2>Complimentary Registration for Volunteers</h2>
@@ -24,9 +217,8 @@
   help out in exchange for no consideration. Therefore, we only offer free
   tickets to people who are willing to commit to <b>at least eight hours</b> of
   volunteering during the event and who are willing to have their volunteer
-  shifts <b>assigned by the organizers</b>. Even if you meet these criteria, we
-  may not be able to accommodate everyone who would like a free ticket. However,
-  if interested, please fill out
-  <a href="https://forms.gle/JnpkGhECfhYooMyv9">this Google Form</a>. We will
-  get back to you within approximately two weeks.
+  shifts <b>assigned by the organizers</b>. We currently do not anticipate
+  awarding any more free tickets to volunteers for this year's event; however,
+  we are still looking for a few more people to fill up our remaining volunteer
+  slots.
 </p>
